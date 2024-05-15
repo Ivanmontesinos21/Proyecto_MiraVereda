99c9cadf081087a4c9b16a58a69bd914f0a4bbe0
Ivanmontesinos21 <118199262+Ivanmontesinos21@users.noreply.github.com>
2024-05-15 19:45:04 +0200

    DDL update to Oracle

--- DDL Mira_Vereda.sql
+++ DDL Mira_Vereda.sql
@@ -1,161 +1,181 @@
-drop database if exists miraVereda;
-create database miraVereda;
-use miraVereda;
+
 
 Create table Tarifa(
-id varchar(30) primary key,
-increment int not null
+id_tarifa varchar2(30) primary key,
+increment_tarifa number not null,
+changedTs timestamp
+
 );
 
 Create table Actor(
-dni varchar(9) primary key,
-nombre varchar(30) not null,
-apellidos varchar(30) not null
+dni varchar2(9) primary key,
+nombre varchar2(30) not null,
+apellidos varchar2(30) not null,
+changedTs timestamp
 );
+create sequence ca_sequence;
 
-Create table Contenido_AudioVisual(
-id int auto_increment primary key,
-genero varchar(30) not null,
+create table Contenido_AudioVisual(
+id_ca integer primary key,
+genero varchar2(30) not null,
 fecha_estreno DATE not null,
 duracion integer not null,
-titulo varchar(20) not null,
-precio double not null,
-descripcion varchar(150) not null,
-valoracion_media double,
-nombre_director varchar(30) not null,
-version_idioma varchar(30)not null,
-id_tarifa_fk varchar(30),
-foreign key (id_tarifa_fk) references Tarifa(id)
+titulo varchar2(20) not null,
+precio integer not null,
+descripcion varchar2(150) not null,
+valoracion_media number(2,1),
+nombre_director varchar2(30) not null,
+version_idioma varchar2(30)not null,
+id_Tarifa varchar2(30),
+changedTs timestamp,
+constraint id_tarifa_fk foreign key (id_Tarifa) references Tarifa(id_tarifa)
+
 );
+
 
 Create table Actuacion_ContenAudiovi(
-dni_actor varchar(9),
-id_conteAudiovi int,
-foreign key(dni_actor) references Actor(dni),
-foreign key(id_conteAudiovi) references Contenido_AudioVisual(id),
-primary key(dni_actor,id_conteAudiovi)
+dni_actor varchar2(9),
+id_conteAudiovi integer,
+constraint dni_actor_fk foreign key(dni_actor) references Actor(dni),
+constraint id_conteAudiovi_fk foreign key(id_conteAudiovi) references Contenido_AudioVisual(id_ca),
+primary key(dni_actor,id_conteAudiovi),
+changedTs timestamp
 );
 
+
+
 Create table Cliente(
-id_cliente varchar(30) primary key,
-nombre varchar(50) not null,
-apellidos varchar(50) not null,
-contraseña varchar(30) not null,
-email varchar(30),
-domicilio varchar(50),
-codigo_postal varchar(7),
+id_cliente varchar2(30) primary key,
+nombre varchar2(50) not null,
+apellidos varchar2(50) not null,
+contraseña varchar2(30) not null,
+email varchar2(30),
+domicilio varchar2(50),
+codigo_postal varchar2(7),
 fecha_nacimiento DATE not null,
-id_contenidoAudiovi int,
-foreign key (id_contenidoAudiovi) references Contenido_AudioVisual(id)
+id_contenidoAudiovi integer,
+changedTs timestamp
 );
 
 Create table Alquila(
-id_conteAudiovi int,
-id_cliente varchar(30),
-foreign key(id_conteAudiovi) references Contenido_AudioVisual(id),
-foreign key(id_cliente) references Cliente(id_cliente),
+id_conteAudiovi integer,
+id_cliente varchar2(30),
+constraint id_conteAudiovi_fk_alquila foreign key(id_conteAudiovi) references Contenido_AudioVisual(id_ca),
+constraint id_cliente_fk_alquila foreign key(id_cliente) references Cliente(id_cliente),
 primary key(id_cliente,id_conteAudiovi),
-cobrado varchar(20) not null,
-facturado varchar(20) not null,
-valoracion double
+cobrado varchar2(20) not null,
+facturado varchar2(20) not null,
+valoracion number(2,1),
+changedTs timestamp
 
 );
-
+create sequence factura_sequence;
 Create table Factura(
-numero_factura int auto_increment primary key,
-importe_base double not null,
-importe_con_iva double not null,
+numero_factura integer  primary key,
+importe_base integer not null,
+importe_con_iva integer not null,
 fecha date not null,
-id_cliente varchar(30),
-foreign key (id_cliente) references Cliente(id_cliente)
+id_cliente varchar2(30),
+constraint id_cliente_factura foreign key (id_cliente) references Cliente(id_cliente),
+changedTs timestamp
 );
 
+create sequence linia_factura_sequencia;
 Create table Linia_Factura(
-numero_factura int auto_increment,
+numero_factura integer,
 id_contenidoAudioVisual int,
-num_linea varchar(30),
-foreign key(numero_factura) references Factura(numero_factura),
+num_linea varchar2(30),
+ constraint numero_factura_fk foreign key(numero_factura) references Factura(numero_factura),
 primary key(numero_factura,num_linea),
-foreign key(id_contenidoAudioVisual) references Contenido_AudioVisual(id)
+constraint id_contenidoAudioVisual_fk_linea_factura foreign key(id_contenidoAudioVisual) references Contenido_AudioVisual(id_ca),
+changedTs timestamp
 );
 
 Create table Pelicula(
 disponible_hasta date not null,
-id_contenidoAudiovisual int,
-genero varchar(30),
+id_contenidoAudiovisual integer,
+genero varchar2(30),
 fecha_estreno DATE,
 duracion integer,
-titulo varchar(20),
-precio double,
-descripcion varchar(150),
-valoracion_media double,
-nombre_director varchar(30),
-version_idioma varchar(30),
+titulo varchar2(20),
+precio integer,
+descripcion varchar2(150),
+valoracion_media number(2,1),
+nombre_director varchar2(30),
+version_idioma varchar2(30),
 id_tarifa varchar(30),
-foreign key (id_tarifa) references Tarifa(id),
-foreign key(id_contenidoAudiovisual) references Contenido_AudioVisual(id),
-primary key(id_contenidoAudiovisual)
+constraint id_tarifa_fk_pelicula foreign key (id_tarifa) references Tarifa(id_tarifa),
+constraint id_contenidoAudiovisual_fk_pelicula foreign key(id_contenidoAudiovisual) references Contenido_AudioVisual(id_ca),
+primary key(id_contenidoAudiovisual),
+changedTs timestamp
 );
 
 Create table Corto(
-id_contenidoAudiovisual int,
-genero varchar(30),
+id_contenidoAudiovisual integer,
+genero varchar2(30),
 fecha_estreno DATE,
 duracion integer,
-titulo varchar(20),
-precio double,
-descripcion varchar(150),
-valoracion_media double,
-nombre_director varchar(30),
-version_idioma varchar(30),
-id_tarifa varchar(30),
-foreign key (id_tarifa) references Tarifa(id),
-foreign key(id_contenidoAudiovisual) references Contenido_AudioVisual(id),
-primary key(id_contenidoAudiovisual)
+titulo varchar2(20),
+precio integer,
+descripcion varchar2(150),
+valoracion_media number(2,1),
+nombre_director varchar2(30),
+version_idioma varchar2(30),
+id_tarifa varchar2(30),
+ constraint id_tarifa_fk_corto foreign key (id_tarifa) references Tarifa(id_tarifa),
+constraint id_contenidoAudiovisual_fk_corto foreign key(id_contenidoAudiovisual) references Contenido_AudioVisual(id_ca),
+primary key(id_contenidoAudiovisual),
+changedTs timestamp
 );
+create sequence serie_sequence;
 
 Create table Serie(
-id_contenidoAudiovisual int,
-titulo varchar(30) not null,
-codigo int auto_increment,
-foreign key(id_contenidoAudiovisual) references Contenido_AudioVisual(id),
-primary key(codigo)
+id_contenidoAudiovisual integer,
+titulo varchar2(30) not null,
+codigo integer ,
+ constraint id_contenidoAudiovisual_fk_serie foreign key(id_contenidoAudiovisual) references Contenido_AudioVisual(id_ca),
+primary key(codigo),
+changedTs timestamp
 );
-
+create sequence capitulo_sequence; 
 Create table Capitulo(
 disponible_desde date not null,
-codigo_Serie int not null,
-temporada varchar(10) not null,
-id_contenidoAudiovisual int auto_increment,
-genero varchar(30),
+codigo_Serie integer not null,
+temporada varchar2(10) not null,
+id_contenidoAudiovisual integer,
+genero varchar2(30),
 fecha_estreno DATE,
 duracion integer,
-titulo varchar(20),
-precio double,
-descripcion varchar(150),
-valoracion_media double,
-nombre_director varchar(30),
-version_idioma varchar(30),
-id_tarifa varchar(30),
-foreign key (id_tarifa) references Tarifa(id),
-foreign key(id_contenidoAudiovisual) references Contenido_AudioVisual(id),
-foreign key(codigo_Serie) references Serie(codigo),
-primary key(id_contenidoAudiovisual)
+titulo varchar2(20),
+precio integer,
+descripcion varchar2(150),
+valoracion_media number(2,1),
+nombre_director varchar2(30),
+version_idioma varchar2(30),
+id_tarifa varchar2(30),
+ constraint id_tarifa_fk_capitulo foreign key (id_tarifa) references Tarifa(id_tarifa),
+constraint id_contenidoAudiovisual_fk_capitulo foreign key(id_contenidoAudiovisual) references Contenido_AudioVisual(id_ca),
+constraint codigo_serie_fk foreign key(codigo_Serie) references Serie(codigo),
+primary key(id_contenidoAudiovisual),
+changedTs timestamp
 );
 
 create table Tarjeta(
-num_tarjeta varchar(9) primary key,
+id_tarjeta varchar2(9) primary key,
+num_tarjeta varchar2(16) not null,
 fecha_caducidad date not null,
-CVV varchar(3) not null,
-titular varchar(20) not null,
-id_cliente varchar(30) not null,
-foreign key (id_cliente) references Cliente(id_cliente)
+CVV varchar2(3) not null,
+titular varchar2(20) not null,
+id_cliente varchar2(30) not null,
+constraint id_cliente_fk_tarjeta foreign key (id_cliente) references Cliente(id_cliente),
+changedTs timestamp
 );
 
 create table pertenece_tarjeta(
-id_cliente varchar(30),
-num_tarjeta varchar(9),
-foreign key (id_cliente) references Cliente(id_cliente),
-foreign key (num_tarjeta) references Tarjeta (num_tarjeta),
-primary key(id_cliente,num_tarjeta)
+id_cliente varchar2(30),
+id_tarjeta varchar2(9),
+constraint id_cliente_fk_pertenece_tarjeta foreign key (id_cliente) references Cliente(id_cliente),
+constraint id_tarjeta_fk foreign key (id_tarjeta) references Tarjeta (id_tarjeta),
+primary key(id_cliente,id_tarjeta),
+changedTs timestamp
 );
